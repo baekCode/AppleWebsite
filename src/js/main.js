75ef8891a77def52c0a6c910cdd7168fdb6a0d15
baekCode <sosaer88@gmail.com>
2020-07-03 22:38:34 +0900

    feat : 1번 섹션 스크롤  애니메이션 준비

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let yOffset = 0;
     let prevScrollHeight = 0;
     let currentScene = 0;
+    let enterNewScene = false;
 
     const sceneInfo = [
       {
@@ -11,6 +12,13 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollHeight: 0,
         obj: {
           container: document.querySelector('#scroll-section-0'),
+          sticky0: document.querySelector('#scroll-section-0 .sticky-0'),
+          sticky1: document.querySelector('#scroll-section-0 .sticky-1'),
+          sticky2: document.querySelector('#scroll-section-0 .sticky-2'),
+          sticky3: document.querySelector('#scroll-section-0 .sticky-3'),
+        },
+        values: {
+          sticky0Opacity: [0, 1],
         },
       },
       {
@@ -44,25 +52,62 @@ window.addEventListener('DOMContentLoaded', () => {
         element.scrollHeight = element.heightNum * window.innerHeight;
         element.obj.container.style.height = `${element.scrollHeight}px`;
       });
+      let totalScrollHeight = 0;
+      for (let i = 0; i < sceneInfo.length; i++) {
+        totalScrollHeight += sceneInfo[i].scrollHeight;
+        if (totalScrollHeight >= yOffset) {
+          currentScene = i;
+          break;
+        }
+      }
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    };
+
+    const calcValues = (values, currentYoffset) => {
+      let scrollRatio = currentYoffset / sceneInfo[currentScene].scrollHeight;
+      return scrollRatio * (values[1] - values[0]) + values[0];
+    };
+
+    const playAnimation = () => {
+      const obj = sceneInfo[currentScene].obj;
+      const values = sceneInfo[currentScene].values;
+      const currentYoffset = yOffset - prevScrollHeight;
+
+      switch (currentScene) {
+        case 0:
+          let sticky0OpacityIn = calcValues(values.sticky0Opacity, currentYoffset);
+          obj.sticky0.style.opacity = sticky0OpacityIn;
+          break;
+        case 0:
+          break;
+        case 0:
+          break;
+        case 0:
+          break;
+        default:
+          break;
+      }
     };
 
     const scrollLoop = () => {
+      enterNewScene = false;
       prevScrollHeight = 0;
       for (let i = 0; i < currentScene; i++) {
         prevScrollHeight += sceneInfo[i].scrollHeight;
       }
-      yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight && currentScene++;
+      yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight && ((enterNewScene = true), currentScene++);
       yOffset < prevScrollHeight && currentScene > 0 && currentScene--;
 
-      console.log(currentScene);
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+      if (enterNewScene) return;
+      playAnimation();
     };
 
+    window.addEventListener('load', setLayout);
     window.addEventListener('resize', setLayout);
     window.addEventListener('scroll', () => {
       yOffset = window.pageYOffset;
       scrollLoop();
     });
-
-    setLayout();
   })();
 });
